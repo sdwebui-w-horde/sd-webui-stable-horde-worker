7ae7ccb19586183c822d3c346c3a83bda77f19b9
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2023-01-23 22:23:56 +0800

    fix: 400 error on submit generation (#26)
    This is part of #25 that fixes the submitting bug and
    add save images features.
    It is better to cherry-pick the bug-fixes to the master branch
    to fix the emergence submitting bug immediately.
    
    Co-authored-by: John Warnes <john@warnes.email>

--- stable_horde.py
+++ stable_horde.py
@@ -293,10 +293,10 @@ class StableHorde:
         else:
             generation = base64.b64encode(bytesio.getvalue()).decode("utf8")
 
-        await self.submit(req['id'], req['payload']['seed'], generation)
+        await self.submit(req['id'], int(req['payload']['seed']), generation)
 
 
-    async def submit(self, id: str, seed: str, generation: str):
+    async def submit(self, id: str, seed: int, generation: str):
         post_data = {
             "id": id,
             "generation": generation,
