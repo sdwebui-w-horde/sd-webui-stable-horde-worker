0fb6f7da05999558ae6ce1ff6c3860bb0049ff35
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2023-01-26 23:35:25 +0800

    fix: remove unexpetectedly merged commits

--- stable_horde.py
+++ stable_horde.py
@@ -116,7 +116,6 @@ class StableHorde:
         while True:
             result = self.detect_current_model()
             if result is not None:
-                self.state.status = result
                 # Wait 10 seconds before retrying to detect the current model
                 # if the current model is not listed in the Stable Horde supported models,
                 # we don't want to spam the server with requests
