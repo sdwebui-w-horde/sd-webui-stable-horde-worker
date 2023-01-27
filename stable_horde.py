e58c05a6d63dd0f54146f6d539ecbe530de4d8e6
Bastian Schülke <bas.schuelke@googlemail.com>
2023-01-27 13:31:41 +0100

    500 server error, multiple delimiter (#48)
    * Getting sometimes 500 errors froom server, but reties upload the image correctly
    
    * Multiple delimiter in prompt, choose the last one in string and continue

--- stable_horde.py
+++ stable_horde.py
@@ -219,6 +219,12 @@ class HordeJob:
                     if r.status == 404:
                         print(f"job {self.id} has been submitted already")
                         return
+                        
+                    if r.status == 500:
+                        print(f"Failed to submit job with status code {r.status}, retry!")
+                        attempts -= 1
+                        await asyncio.sleep(self.retry_interval)
+                        continue
 
                     if r.ok:
                         return res.get("reward", None)
@@ -277,7 +283,7 @@ class HordeJob:
         payload = req.get("payload")
         prompt = payload.get("prompt")
         if "###" in prompt:
-            prompt, negative = map(lambda x: x.strip(), prompt.split("###"))
+            prompt, negative = map(lambda x: x.strip(), prompt.rsplit("###", 1))
         else:
             negative = ""
 
